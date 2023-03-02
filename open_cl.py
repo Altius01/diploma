276c879c79dcfd92e788577008fb4a397cb142b3
altius01 <altius01@mail.ru>
2023-03-02 14:22:35 +0300

    Hotfix_int

--- open_cl.py
+++ open_cl.py
@@ -332,6 +332,7 @@ def compute_kin_energy(knl, ctx, mf, queue, rho_gpu, u_gpu):
     
     cl.enqueue_copy(queue, sums, partialSums)
 
+  print(result, sums[0])
   assert(np.allclose([result, ], [sums[0], ]))
   return sums[0]
 
